53e1ec4d470f9b785246c23c0fd10ca87b503f99
ptlharshil <patelharshil3695@gmail.com>
2021-05-19 14:14:33 -0400

    Server and db connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,13 @@
 const express=require("express");
 const mongoose= require("mongoose");
+const dotenv= require("dotenv");
+
 const app= express();
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology:true}).then(()=>{
+    console.log("MongoDB is connected")
+}).catch((err)=>console.log(err));
 
 app.listen(8000, ()=>{
-    console.log("backend");
+    console.log("backend started");
 })
