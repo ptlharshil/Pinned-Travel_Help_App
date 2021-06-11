31b19f894e91671559f422a6b9c73bd08a7dc08c
ptlharshil <patelharshil3695@gmail.com>
2021-06-11 00:07:06 -0400

    Homepage changed

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -3,8 +3,11 @@ import { Room } from '@material-ui/icons'
 import React from 'react'
 import "./Home.css"
 import {Link} from "react-scroll"
+
 const HomePage = () => {
     return (
+        <>
+        
         <div className="home" style={{ height: "100vh", width: "100%"}}>
             
             <h1 className="title"><Room className="loc"/><b>Pinned</b></h1>
@@ -23,7 +26,12 @@ const HomePage = () => {
             duration={500}
             className="buttonExplore" ><b>Explore</b>   
             </Link>
+            <div id="earth"></div>
         </div>
+        
+
+        
+        </>
     )
 }
 
